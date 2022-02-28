f08bbb992abb7364912660222e4b1569dcb7a007
aartaka <aartaka@protonmail.com>
2022-02-28 20:43:37 +0500

    readability-plump: Add several Readability-like utility methods.

--- readability-plump.lisp
+++ readability-plump.lisp
@@ -36,3 +36,46 @@
           when (> max-score min-score)
             do (return t)
           finally (return nil))))
+
+;; XXX: A free rewrite of Readability._cleanClasses()
+(defmethod normalize-classes ((node plump:node))
+  ;; TODO: Classes to preserve.
+  (when (plump:element-p node)
+    (plump:remove-attribute node "class")
+    (loop for child across (plump:children node)
+          do (normalize-classes child))))
+
+;;; XXX: Readability._fixRelativeUris
+(defmethod fix-relative-urls ((node plump:element) absolute-uri)
+  (flet ((relative->absolute (uri)
+           (quri:merge-uris (quri:uri uri) (quri:uri absolute-uri))))
+    (loop for elem across (clss:select "a, img, picture, figure, video, audio, source" node)
+          for href = (plump:get-attribute elem "href")
+          for src = (plump:get-attribute elem "src")
+          for poster = (plump:get-attribute elem "poster")
+          ;; TODO: srcset
+          unless (uiop:emptyp src)
+            do (plump:set-attribute elem "src" (relative->absolute src))
+          unless (uiop:emptyp poster)
+            do (plump:set-attribute elem "poster" (relative->absolute poster))
+          when (and href (uiop:string-prefix-p "JavaScript:" href))
+            ;; TODO: if the link only contains simple text content, it
+            ;; can be converted to a text node
+            ;;
+            ;; Replace the link with a span to preserve children.
+            do (plump:replace-child
+                elem (make-instance
+                      'plump:element :tag-name "span" :parent nil :children (plump:children elem)))
+          else do (plump:set-attribute elem "href" (relative->absolute href)))))
+
+;;; XXX: Readability._simplifyNestedElements
+(defmethod simplify-nested-elements ((element plump:element))
+  (loop for node = element then (get-next-node node)
+        for parent = (plump:parent node)
+        when (and (clss:node-matches-p "div, section" node)
+                  (without-content-p node))
+          do (plump:remove-child node)
+        else when (serapeum:single (plump:children node))
+               do (plump:replace-child
+                   node (serapeum:lret ((child (elt (plump:children node) 0)))
+                          (setf (plump:attributes child) (plump:attributes node))))))
