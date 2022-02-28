adc29601dfa1c16e76586a834e44aae35badfaaf
aartaka <aartaka@protonmail.com>
2022-02-28 18:50:38 +0500

    Change package-name to a shorter "readability".

--- package.lisp
+++ package.lisp
@@ -1,6 +1,6 @@
 ;;;; SPDX-FileCopyrightText: Atlas Engineer LLC
 ;;;; SPDX-License-Identifier: BSD-3-Clause
 
-(defpackage #:cl-readability
+(defpackage #:readability
   (:use #:cl)
   (:import-from #:serapeum #:export-always))
--- readability.lisp
+++ readability.lisp
@@ -1,7 +1,7 @@
 ;;;; SPDX-FileCopyrightText: Atlas Engineer LLC
 ;;;; SPDX-License-Identifier: BSD-3-Clause
 
-(in-package #:cl-readability)
+(in-package #:readability)
 
 (defvar *default-min-content-length* 140)
 
